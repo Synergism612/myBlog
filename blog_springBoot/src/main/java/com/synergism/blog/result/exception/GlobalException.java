138e943eb5c889108bfa5da984878c211eb81502
Synergism612 <58223490+Synergism612@users.noreply.github.com>
2022-08-25 19:15:36 +0800

    全局异常处理

--- blog_springBoot/src/main/java/com/synergism/blog/result/exception/GlobalException.java
+++ blog_springBoot/src/main/java/com/synergism/blog/result/exception/GlobalException.java
@@ -6,12 +6,20 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * 全局异常处理
+ */
 @ControllerAdvice
 public class GlobalException extends RuntimeException{
 
+    /**
+     * 拦截IllegalArgumentException异常
+     * @param e 异常
+     * @return 结果
+     */
     @ExceptionHandler(value =IllegalArgumentException.class)
     @ResponseBody
-    public Result<Object> exceptionHandler(IllegalArgumentException e){
+    public Result<Object> IllegalArgumentException(IllegalArgumentException e){
         return Result.error(CodeMsg.BIND_ERROR.fillArgs(e.getMessage()));
     }
 }
