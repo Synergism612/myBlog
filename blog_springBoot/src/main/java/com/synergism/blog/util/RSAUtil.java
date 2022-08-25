450cce58d6b62978dcea8caaa1798d93f669e27e
Synergism612 <58223490+Synergism612@users.noreply.github.com>
2022-08-25 16:14:40 +0800

    RSA非对称加密

---
+++ blog_springBoot/src/main/java/com/synergism/blog/util/RSAUtil.java
@@ -0,0 +1,82 @@
+package com.synergism.blog.util;
+
+import org.apache.tomcat.util.codec.binary.Base64;
+
+import javax.crypto.Cipher;
+import java.security.*;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RSAUtil {
+    /**
+     * 随机生成密钥对
+     * @throws NoSuchAlgorithmException
+     * @return
+     */
+    public static Map<String, String> getKeyPair() throws NoSuchAlgorithmException {
+        Map<String, String> result = new HashMap<>();
+        //KeyPairGenerator对象用于生成密钥对，基于RSA算法生成对象
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        //初始化生成器，使用给定的随机源（和默认的参数集合）初始化确定密钥大小的密钥对生成器
+        keyPairGenerator.initialize(1024, SecureRandom.getInstanceStrong());
+        //生成密钥对存放在KeyPair中
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+        //得到私钥
+        RSAPrivateKey privateKey = (RSAPrivateKey)keyPair.getPrivate();
+        //得到公钥
+        RSAPublicKey publicKey = (RSAPublicKey)keyPair.getPublic();
+        //得到公钥字符串
+        String publicKeyString = new String(Base64.encodeBase64(publicKey.getEncoded()));
+        //得到私钥字符串
+        String privateKeyString = new String(Base64.encodeBase64((privateKey.getEncoded())));
+
+        result.put("public",publicKeyString);  //存放公钥
+        result.put("private",privateKeyString);  //存放私钥
+
+        return result; //返回密钥对
+    }
+    /**
+     * RSA公钥加密
+     *
+     * @param str 加密字符串
+     * @param publicKey 公钥
+     * @return 密文
+     * @throws Exception 加密过程中的异常信息
+     */
+    public static String encrypt( String str, String publicKey ) throws Exception{
+        //base64编码的公钥
+        byte[] decoded = Base64.decodeBase64(publicKey);
+        RSAPublicKey pubKey = (RSAPublicKey) KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(decoded));
+        //RSA加密
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, pubKey);
+        String outStr = Base64.encodeBase64String(cipher.doFinal(str.getBytes("UTF-8")));
+        return outStr;
+    }
+
+    /**
+     * RSA私钥解密
+     *
+     * @param str 加密字符串
+     * @param privateKey 私钥
+     * @return 铭文
+     * @throws Exception 解密过程中的异常信息
+     */
+    public static String decrypt(String str, String privateKey) throws Exception{
+        //64位解码加密后的字符串
+        byte[] inputByte = Base64.decodeBase64(str.getBytes("UTF-8"));
+        //base64编码的私钥
+        byte[] decoded = Base64.decodeBase64(privateKey);
+        RSAPrivateKey priKey = (RSAPrivateKey) KeyFactory.getInstance("RSA").generatePrivate(new PKCS8EncodedKeySpec(decoded));
+        //RSA解密
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, priKey);
+        String outStr = new String(cipher.doFinal(inputByte));
+        return outStr;
+    }
+
+}
--- blog_springBoot/src/test/java/com/synergism/blog/BlogApplicationTests.java
+++ blog_springBoot/src/test/java/com/synergism/blog/BlogApplicationTests.java
@@ -2,10 +2,14 @@ package com.synergism.blog;
 
 import com.synergism.blog.entity.Aboutblog;
 import com.synergism.blog.mapper.AboutblogMapper;
+import com.synergism.blog.util.RSAUtil;
 import com.synergism.blog.util.base64Util;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.Map;
 
 @SpringBootTest
 class BlogApplicationTests {
@@ -33,5 +37,17 @@ class BlogApplicationTests {
 		System.out.println(src);
 		System.out.println(encode);
 		System.out.println(decode);
+	}
+
+	@Test
+	void RSACodeTest() throws Exception {
+		String message = "synergism612731";
+		Map<String, String> keyMap = RSAUtil.getKeyPair();
+		System.out.println("随机生成的公钥为:" + keyMap.get("public"));
+		System.out.println("随机生成的私钥为:" + keyMap.get("private"));
+		String messageEn = RSAUtil.encrypt(message,keyMap.get("public"));
+		System.out.println(message + "\t加密后的字符串为:" + messageEn);
+		String messageDe = RSAUtil.decrypt(messageEn,keyMap.get("private"));
+		System.out.println("还原后的字符串为:" + messageDe);
 	}
 }
