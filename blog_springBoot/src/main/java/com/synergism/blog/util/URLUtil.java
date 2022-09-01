f85c78edaf7eb648890ec191f05f05fa12f18b72
Synergism612 <58223490+Synergism612@users.noreply.github.com>
2022-09-02 00:02:00 +0800

    URL工具类

---
+++ blog_springBoot/src/main/java/com/synergism/blog/util/URLUtil.java
@@ -0,0 +1,8 @@
+package com.synergism.blog.util;
+
+public class URLUtil {
+
+    public static boolean checkURLContain(String url,String contain){
+        return StringUtil.checkStringContain(url, contain);
+    }
+}
