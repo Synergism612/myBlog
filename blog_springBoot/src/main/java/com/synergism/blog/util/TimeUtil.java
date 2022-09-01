10d493db47975d475fb15ae78fb39450bb41a6cc
Synergism612 <58223490+Synergism612@users.noreply.github.com>
2022-09-02 00:01:48 +0800

    时间工具类

--- blog_springBoot/src/main/java/com/synergism/blog/util/TimeUtil.java
+++ blog_springBoot/src/main/java/com/synergism/blog/util/TimeUtil.java
@@ -1,12 +1,12 @@
 package com.synergism.blog.util;
 
-import com.synergism.blog.enums.TimeEnum;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import static com.synergism.blog.enums.TimeEnum.format;
+
 public class TimeUtil {
     public static String now(){
-        return new SimpleDateFormat(TimeEnum.format()).format(new Date());
+        return new SimpleDateFormat(format()).format(new Date());
     }
 }
