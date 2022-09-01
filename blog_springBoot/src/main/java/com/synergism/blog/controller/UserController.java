cd2c197ab8f534139b4614398a177169a5631e0c
Synergism612 <58223490+Synergism612@users.noreply.github.com>
2022-09-02 00:33:39 +0800

    改用构造函数方式注入

--- blog_springBoot/src/main/java/com/synergism/blog/controller/UserController.java
+++ blog_springBoot/src/main/java/com/synergism/blog/controller/UserController.java
@@ -2,10 +2,9 @@ package com.synergism.blog.controller;
 
 import com.synergism.blog.entity.Login;
 import com.synergism.blog.service.UserService;
+import com.synergism.blog.serviceImpl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.synergism.blog.util.StringUtil.checkStringIsEmpty;
 import static com.synergism.blog.util.StringUtil.checkStringLength;
@@ -23,8 +22,12 @@ import static com.synergism.blog.util.StringUtil.checkStringLength;
 @RequestMapping("/user")
 public class UserController {
 
+    private UserService service;
+
     @Autowired
-    UserService service;
+    UserController(UserService service){
+        this.service =service;
+    }
 
     @PostMapping("/login")
     public void login(Login login) {
