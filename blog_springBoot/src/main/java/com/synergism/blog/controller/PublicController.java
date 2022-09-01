5b3ddc80441d70ee506e7f09ab810ddd118121fc
Synergism612 <58223490+Synergism612@users.noreply.github.com>
2022-09-01 22:07:04 +0800

    获取公钥api

---
+++ blog_springBoot/src/main/java/com/synergism/blog/controller/PublicController.java
@@ -0,0 +1,22 @@
+package com.synergism.blog.controller;
+
+import com.synergism.blog.enums.RSAEnum;
+import com.synergism.blog.result.entity.Result;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/public")
+public class PublicController {
+
+    /**
+     * 请求获取公钥
+     * @return 公钥
+     */
+    @GetMapping("/key")
+    public Result<String> getKey() {
+        return Result.success(System.getProperty(RSAEnum.PUBLIC()));
+    }
+
+}
