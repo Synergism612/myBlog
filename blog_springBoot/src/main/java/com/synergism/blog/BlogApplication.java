c38eb37880265fac927827fa5860732b7a91e96b
Synergism612 <58223490+Synergism612@users.noreply.github.com>
2022-09-01 22:05:57 +0800

    非对称加密初始化

--- blog_springBoot/src/main/java/com/synergism/blog/BlogApplication.java
+++ blog_springBoot/src/main/java/com/synergism/blog/BlogApplication.java
@@ -4,11 +4,18 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.security.NoSuchAlgorithmException;
+
+import static com.synergism.blog.Init.init;
+
 @SpringBootApplication
 @MapperScan("com/synergism/blog/mapper")
 public class BlogApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoSuchAlgorithmException {
+		//初始化
+		init();
+		//运行
 		SpringApplication.run(BlogApplication.class, args);
 	}
 
---
+++ blog_springBoot/src/main/java/com/synergism/blog/Init.java
@@ -0,0 +1,15 @@
+package com.synergism.blog;
+
+import com.synergism.blog.util.RSAUtil;
+
+import java.security.NoSuchAlgorithmException;
+
+public class Init {
+    /**
+     * 用于初始化
+     */
+    public static void init() throws NoSuchAlgorithmException {
+        //初始化非对称密钥对
+        RSAUtil.creatKeyPair();
+    }
+}
