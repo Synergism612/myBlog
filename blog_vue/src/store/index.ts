c74d30c79f4cb53fce853da43a7cfce4621c143c
Synergism612 <58223490+Synergism612@users.noreply.github.com>
2022-09-04 00:51:14 +0800

    增加key

--- blog_vue/src/store/index.ts
+++ blog_vue/src/store/index.ts
@@ -1,18 +1,16 @@
 import { createStore } from "vuex";
-import AESUtil from "@/utils/AESUtil";
-import RSAUtil from "@/utils/RSAUtil";
 import StoreUtil from "@/utils/StoreUtil";
-import { api } from "@/api/api";
 
 /**
  * 数据仓库
  */
-export default createStore({
+const store = createStore({
   //存放数据
   state: {
     // 检查本地记录是否包含
     ANOTHER_WORLD_KEY: StoreUtil.fetch("ANOTHER_WORLD_KEY") || "",
     PUBLIC_KEY: StoreUtil.fetch("PUBLIC_KEY") || "",
+    KEY: StoreUtil.fetch("KEY") || "",
   },
   //同步函数
   mutations: {
@@ -29,11 +27,31 @@ export default createStore({
     /**
      * 为state.ANOTHER_WORLD_KEY赋值并存储
      * @param state state
-     * @param PUBLIC_KEY 密钥
+     * @param PUBLIC_KEY 公钥加密密钥
      */
     SET_PUBLIC_KEY: (state, PUBLIC_KEY: string) => {
       state.PUBLIC_KEY = PUBLIC_KEY;
-      StoreUtil.save("ANOTHER_WORLD_KEY", PUBLIC_KEY);
+      StoreUtil.save("PUBLIC_KEY", PUBLIC_KEY);
+    },
+
+    /**
+     * 为state.KEY赋值并存储
+     * @param state state
+     * @param KEY 密钥
+     */
+    SET_KEY: (state, KEY: string) => {
+      state.KEY = KEY;
+      StoreUtil.save("KEY", KEY);
+    },
+
+    DELECT_ALL_KEY: (state) => {
+      state.KEY = "";
+      state.PUBLIC_KEY = "";
+      state.ANOTHER_WORLD_KEY = "";
+      StoreUtil.save("KEY", "");
+      StoreUtil.save("PUBLIC_KEY", "");
+      StoreUtil.save("ANOTHER_WORLD_KEY", "");
+
     },
   },
   //异步函数
@@ -41,3 +59,5 @@ export default createStore({
   //分类管理
   modules: {},
 });
+
+export default store;
