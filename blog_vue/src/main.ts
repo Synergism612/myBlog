6e13b7a67824058b850813b11dfd7626e40f69d9
Synergism612 <58223490+Synergism612@users.noreply.github.com>
2022-09-03 11:53:07 +0800

    main ts 重构 并引入全局api

--- blog_vue/src/main.ts
+++ blog_vue/src/main.ts
@@ -12,7 +12,7 @@ import ElementPlus from "element-plus";
  * 引入axios，配置$axios。
  * 在页面中就可以用this.$axios来获取axios
  */
-import axios from "axios";
+import axios from "@/axios/axios";
 
 const app = createApp(App);
 app.use(router).use(store).use(ElementPlus);
