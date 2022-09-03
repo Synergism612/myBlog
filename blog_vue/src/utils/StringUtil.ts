22dd9a4061de56a21b5c7a0053ea9663bccd4d1e
Synergism612 <58223490+Synergism612@users.noreply.github.com>
2022-09-04 00:52:20 +0800

    字符串判空

--- blog_vue/src/utils/StringUtil.ts
+++ blog_vue/src/utils/StringUtil.ts
@@ -6,13 +6,27 @@ export default class StringUtil {
    * @param str 字符串
    * @return 为空则真，反之则假
    */
-  public static checkStringIfEmpty(str: string): boolean {
+  public static checkStringIfEmpty(str: string | null | undefined): boolean {
     return (
       str == null ||
       str.length == 0 ||
       str == "null" ||
       str == undefined ||
-      str == ""
+      str == "" ||
+      str == "none" ||
+      str == "undefined"
     );
+  }
+
+  /**
+   * @param str 字符串
+   * @return 为空则真，反之则假
+   */
+  public static checkStringsIfEmpty(strs: string[]): boolean {
+    let result = false;
+    strs.forEach((str) => {
+      result = this.checkStringIfEmpty(str);
+    });
+    return result;
   }
 }
