3e30e7862fb4d27dc10c6da89d1be3384a0b848e
Synergism612 <58223490+Synergism612@users.noreply.github.com>
2022-09-04 00:49:51 +0800

    获取公钥

--- blog_vue/src/api/api.ts
+++ blog_vue/src/api/api.ts
@@ -1,15 +1,35 @@
 import axios from "@/axios/axios";
-// import { Result } from "@/result/Result";
+import store from "@/store";
+import AESUtil from "@/utils/AESUtil";
+import StringUtil from "@/utils/StringUtil";
 
 export class api {
   public static getPublicKey(): string {
-    //导出方法
+    //开启请求
     axios({
       url: "/api/public/key",
       method: "get",
     })
       .then(({ data }) => {
-        return data;
+        //判断store中是否存在钥匙,若不存在,初始化钥匙
+        if (
+          StringUtil.checkStringsIfEmpty([
+            store.state.ANOTHER_WORLD_KEY,
+            store.state.PUBLIC_KEY,
+            store.state.KEY,
+          ])
+        ) {
+          //更改公钥值
+          store.commit("SET_PUBLIC_KEY", data);
+          //生成钥匙
+          const key = AESUtil.getKey();
+          store.commit("SET_KEY", key);
+          store.commit(
+            "SET_ANOTHER_WORLD_KEY",
+            //公钥加密密钥
+            AESUtil.encrypt(key, store.state.PUBLIC_KEY)
+          );
+        }
       })
       .catch((err) => {
         console.log("err:\n" + err);
