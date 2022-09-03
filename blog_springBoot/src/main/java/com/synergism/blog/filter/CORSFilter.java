a57f1520292fd4e0aa4f69ada9c17ff943a25dab
Synergism612 <58223490+Synergism612@users.noreply.github.com>
2022-09-03 18:00:26 +0800

    过滤器跨域处理

---
+++ blog_springBoot/src/main/java/com/synergism/blog/filter/CORSFilter.java
@@ -0,0 +1,43 @@
+package com.synergism.blog.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 跨域处理过滤器
+ */
+@Component
+public class CORSFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    /**
+     * 为响应添加CORS
+     * @param servletRequest 请求
+     * @param servletResponse 响应
+     * @param filterChain 过滤器
+     */
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+        res.addHeader("Access-Control-Allow-Credentials", "true");
+        res.addHeader("Access-Control-Allow-Origin", "http://localhost:8080"); //仅仅只允许该url访问
+        res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+        res.addHeader("Access-Control-Allow-Headers", "Content-Type,X-CAF-Authorization-Token,sessionToken,X-TOKEN");
+        if (((HttpServletRequest) servletRequest).getMethod().equals("OPTIONS")) {
+            servletResponse.getWriter().println("ok");
+            return;
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
