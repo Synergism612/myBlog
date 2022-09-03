dc4b9cfaa7173b725db34dd31d38eee0ad53ab81
Synergism612 <58223490+Synergism612@users.noreply.github.com>
2022-09-04 00:54:32 +0800

    添加app挂载

--- blog_vue/src/App.vue
+++ blog_vue/src/App.vue
@@ -6,12 +6,27 @@
   <router-view />
 </template>
 
+<script lang="ts">
+import { defineComponent } from "vue";
+import { api } from "./api/api";
+
+/**
+ * app组件将会第一个创建,我用来初始化密钥
+ */
+export default defineComponent({
+  setup() {
+    api.getPublicKey();
+  },
+});
+</script>
+
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
