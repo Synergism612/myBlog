4fe204f2c8f4c038c99974590297371c7a7c2386
Synergism612 <58223490+Synergism612@users.noreply.github.com>
2022-09-03 18:02:03 +0800

    结果类封装

---
+++ blog_vue/src/entity/Result.ts
@@ -0,0 +1,24 @@
+import { AxiosResponse } from "axios";
+
+export default class Result {
+  code!: number;
+  msg!: string;
+  time!: string;
+  data = {};
+
+  constructor(code: number, msg: string, time: string, data: { a?: string }) {
+    this.code = code;
+    this.msg = msg;
+    this.time = time;
+    this.data = data;
+  }
+
+  static getResult(response: AxiosResponse): Result {
+    return new Result(
+      response.data.code,
+      response.data.msg,
+      response.data.time,
+      response.data.data
+    );
+  }
+}
