b848256990eab30378bb70eb3eab9151d69b9be2
Synergism612 <58223490+Synergism612@users.noreply.github.com>
2022-09-04 00:52:42 +0800

    自定义请求头

--- blog_vue/src/axios/axios.ts
+++ blog_vue/src/axios/axios.ts
@@ -1,10 +1,8 @@
 import { apiEnum } from "@/enum/apiEnum";
 import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
-import { ElNotification } from "element-plus";
 import Result from "@/entity/Result";
-import StoreUtil from "@/utils/StoreUtil";
-// import store from "./store";
-// import router from "./router";
+import store from "@/store";
+import AESUtil from "@/utils/AESUtil";
 
 //请求根路径
 const baseURL = "http://localhost:8088";
@@ -18,10 +16,24 @@ class Axios {
     timeout: apiEnum.TIMEOUT,
     //跨域设置
     withCredentials: true,
-    //添加自定义头部
+    //自定义头部
     headers: {
-      ANOTHER_WORLD_KEY: StoreUtil.fetch("ANOTHER_WORLD_KEY"),
+      ANOTHER_WORLD_KEY: " ",
     },
+    // `transformRequest` 允许在向服务器发送前，修改请求数据
+    // transformRequest: [
+    //   (data: string) => {
+    //     // 对 data 进行加密
+    //     return AESUtil.encrypt(JSON.stringify(data), store.state.KEY);
+    //   },
+    // ],
+    // `transformResponse` 在传递给 then/catch 前，允许修改响应数据
+    // transformResponse: [
+    //   (data: string) => {
+    //     // 对 data 进行解密
+    //     return AESUtil.decrypt(JSON.stringify(data), store.state.KEY);
+    //   },
+    // ],
   };
 
   // 创建axios对象
@@ -31,13 +43,17 @@ class Axios {
     this.service = axios.create(this.config);
     //请求拦截
     this.service.interceptors.request.use((request: AxiosRequestConfig) => {
-      console.log("请求拦截");
+      if (request && request.headers) {
+        // 多一步判断
+        request.headers["ANOTHER_WORLD_KEY"] = store.state.ANOTHER_WORLD_KEY;
+      }
+      console.log("请求拦截:" + request.url);
       return request;
     });
 
     //响应拦截
     this.service.interceptors.response.use((response: AxiosResponse) => {
-      console.log("响应拦截");
+      console.log("响应拦截:" + response.data.data);
       return Result.getResult(response);
     });
   }
