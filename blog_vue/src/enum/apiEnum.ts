9114ac99369b400c0761352c79ac1073625742a7
Synergism612 <58223490+Synergism612@users.noreply.github.com>
2022-09-04 23:26:00 +0800

    api枚举类

--- blog_vue/src/enum/apiEnum.ts
+++ blog_vue/src/enum/apiEnum.ts
@@ -1,5 +1,5 @@
-export enum apiEnum{
-  TIMEOUT = 2*10000, //超时时间 二十秒
+export enum apiEnum {
+  TIMEOUT = 2 * 10000, //超时时间 二十秒
   OVERDUE = 600, // 登录失效
   FAIL = 999, // 请求失败
   SUCCESS = 200, // 请求成功
