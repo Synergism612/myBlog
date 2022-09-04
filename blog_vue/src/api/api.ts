dd85368e6735ca638b606a7b8da7030ff5ef5bc4
Synergism612 <58223490+Synergism612@users.noreply.github.com>
2022-09-04 23:23:00 +0800

    更新api

--- blog_vue/src/api/api.ts
+++ blog_vue/src/api/api.ts
@@ -1,14 +1,18 @@
 import axios from "@/axios/axios";
-import store from "@/store";
+import { store } from "@/store";
 import AESUtil from "@/utils/AESUtil";
+import RSAUtil from "@/utils/RSAUtil";
 import StringUtil from "@/utils/StringUtil";
 
 export class api {
-  public static getPublicKey(): string {
+  public static getPublicKey(): Promise<void> {
     //开启请求
-    axios({
+    return axios({
       url: "/api/public/key",
       method: "get",
+      data: {
+        id: "152",
+      },
     })
       .then(({ data }) => {
         //判断store中是否存在钥匙,若不存在,初始化钥匙
@@ -21,15 +25,35 @@ export class api {
         ) {
           //更改公钥值
           store.commit("SET_PUBLIC_KEY", data);
+          console.log("公钥钥  " + data);
           //生成钥匙
           const key = AESUtil.getKey();
+          console.log("对称钥  " + key);
           store.commit("SET_KEY", key);
+          console.log("加密对称钥  " + RSAUtil.encryptedData(key, data));
           store.commit(
             "SET_ANOTHER_WORLD_KEY",
             //公钥加密密钥
             AESUtil.encrypt(key, store.state.PUBLIC_KEY)
           );
         }
+      })
+      .catch((err) => {
+        console.log("err:\n" + err);
+      });
+  }
+
+  public static getTest(): string {
+    //开启请求
+    axios({
+      url: "/api/public/test",
+      method: "post",
+      data: {
+        id: "152",
+      },
+    })
+      .then(({ data }) => {
+        console.log(data);
       })
       .catch((err) => {
         console.log("err:\n" + err);
