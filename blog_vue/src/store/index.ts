427f77438f0f58ddaabcfceb858ac726d794644c
Synergism612 <58223490+Synergism612@users.noreply.github.com>
2022-09-09 20:15:29 +0800

    前端小修改

--- blog_vue/src/store/index.ts
+++ blog_vue/src/store/index.ts
@@ -1,6 +1,5 @@
 import { createStore } from "vuex";
 import StoreUtil from "@/utils/StoreUtil";
-import Base64Util from "@/utils/Base64Util";
 
 /**
  * 数据仓库
@@ -12,6 +11,7 @@ export const store = createStore({
     ANOTHER_WORLD_KEY: StoreUtil.fetch("ANOTHER_WORLD_KEY") || "",
     PUBLIC_KEY: StoreUtil.fetch("PUBLIC_KEY") || "",
     KEY: StoreUtil.fetch("KEY") || "",
+    AUTH_ID: StoreUtil.fetch("AUTH_ID") || "",
   },
   //同步函数
   mutations: {
@@ -27,9 +27,9 @@ export const store = createStore({
     },
 
     /**
-     * 为state.ANOTHER_WORLD_KEY赋值并存储
+     * 为state.PUBLIC_KEY赋值并存储
      * @param state state
-     * @param PUBLIC_KEY 公钥加密密钥
+     * @param PUBLIC_KEY 公钥
      */
     SET_PUBLIC_KEY: (state, PUBLIC_KEY: string) => {
       // PUBLIC_KEY = Base64Util.encode(PUBLIC_KEY);
@@ -49,16 +49,26 @@ export const store = createStore({
     },
 
     /**
+     * 为state.AUTH_ID赋值并存储
+     * @param state state
+     * @param KEY 密钥
+     */
+    SET_AUTH_ID: (state, AUTH_ID: string) => {
+      // KEY = Base64Util.encode(KEY);
+      state.AUTH_ID = AUTH_ID;
+      StoreUtil.save("AUTH_ID", AUTH_ID);
+    },
+    /**
      * 删除state中的对称钥和加密对称钥
      * @param state state
      */
     DELECT_ALL_KEY: (state) => {
       console.log("删除");
       state.KEY = "";
-      state.PUBLIC_KEY = "";
+      state.AUTH_ID = "";
       state.ANOTHER_WORLD_KEY = "";
       StoreUtil.save("KEY", "");
-      StoreUtil.save("PUBLIC_KEY", "");
+      StoreUtil.save("AUTH_ID", "");
       StoreUtil.save("ANOTHER_WORLD_KEY", "");
     },
   },
