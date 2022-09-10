74a6ec5e08a4d73c02eeb256e50521f9e6100d39
Synergism612 <58223490+Synergism612@users.noreply.github.com>
2022-09-10 16:17:09 +0800

    vue-login:create vue and less

---
+++ blog_vue/src/views/login/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="background">asdf</div>
+  <div class="login">asdf</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from "vue";
+
+export default defineComponent({
+  /**
+   * 组件内setup，setup最早创建，所以没有this
+   */
+  setup() {
+    const data = reactive({
+      msg: "aa",
+    });
+
+    return { ...toRefs(data) };
+  },
+});
+</script>
+<style lang="less" scoped>
+@import url(./Login.less);
+</style>
