e477514e39b5e0da58cdbd35aeeb69b872cbf995
Synergism612 <58223490+Synergism612@users.noreply.github.com>
2022-09-10 16:14:43 +0800

    vue-all:create all.less

--- blog_vue/src/App.vue
+++ blog_vue/src/App.vue
@@ -1,16 +1,3 @@
 <template>
-  <div id="nav">
-  </div>
   <router-view />
 </template>
-
-<style lang="stylus">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
