689a36f023858a22ff678a7ceeafbf5736cd241a
Synergism612 <58223490+Synergism612@users.noreply.github.com>
2022-09-14 16:10:30 +0800

    vue-menu:update index path

--- blog_vue/src/components/Menu/Menu.vue
+++ blog_vue/src/components/Menu/Menu.vue
@@ -2,13 +2,13 @@
   <div id="menu_container">
     <el-menu mode="horizontal" :ellipsis="ellipsis" router>
       <el-menu-item id="menu_logo" index="/blog/index">Synergism</el-menu-item>
-      <el-menu-item index="2">网站首页</el-menu-item>
+      <el-menu-item index="/blog/index">网站首页</el-menu-item>
       <el-menu-item index="3">文章专栏</el-menu-item>
       <el-menu-item index="4">归档</el-menu-item>
       <el-menu-item index="3">资源分享</el-menu-item>
       <el-menu-item index="3">点点滴滴</el-menu-item>
       <el-menu-item index="3">关于本站</el-menu-item>
-      <el-menu-item index="3" v-if="!isLogin">登录</el-menu-item>
+      <el-menu-item index="/blog/login" v-if="!isLogin">登录</el-menu-item>
       <el-menu-item index="3" v-if="!isLogin">注册</el-menu-item>
       <el-sub-menu v-if="isLogin">
         <template #title>用户名</template>
