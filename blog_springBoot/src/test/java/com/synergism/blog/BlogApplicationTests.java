8379524ef8365f2e807e9a67fd2fd2d6fe620310
Synergism612 <58223490+Synergism612@users.noreply.github.com>
2022-09-15 16:13:02 +0800

    spb-mail:Importing and testing

--- blog_springBoot/src/test/java/com/synergism/blog/BlogApplicationTests.java
+++ blog_springBoot/src/test/java/com/synergism/blog/BlogApplicationTests.java
@@ -6,6 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 @SpringBootTest
 class BlogApplicationTests {
@@ -50,4 +56,34 @@ class BlogApplicationTests {
 		Object o = service.getValue("409379755675090944");
 		System.out.println(o.toString());
 	}
+
+	@Autowired
+	JavaMailSenderImpl mailSender;
+
+	@Test
+	public void contextLoads() {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setSubject("今晚开会");
+		message.setText("大家，好！\n今晚7:30在教学楼201开班委会，请各位班委准时参加！ \n谢谢！");
+		message.setTo("1273059247@qq.com");
+		message.setFrom("synergism2022@163.com");
+		mailSender.send(message);
+	}
+
+	@Test
+	public void test2() throws Exception{
+		MimeMessage mimeMessage = mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage,true);
+		helper.setSubject("今晚开会");
+		helper.setText("大家，好！<br> &nbsp;&nbsp;<b style='color:red'>今晚7:30在教学楼201开班委会，请各位班委准时参加！</b> <br>谢谢！",true);
+		helper.setTo("1273059247@qq.com");
+		helper.setFrom("synergism2022@163.com");
+
+		//添加附件
+		helper.addAttachment("会议说明.txt",new File("C:\\Users\\Dylan\\Pictures\\会议说明.txt"));
+		helper.addAttachment("会议图片.jpg",new File("C:\\Users\\Dylan\\Pictures\\会议图片.jpg"));
+
+		mailSender.send(mimeMessage);
+	}
+
 }
