8058ce5a11921ccd2ee08c95479835ffa43ba528
Synergism612 <58223490+Synergism612@users.noreply.github.com>
2022-09-19 08:55:33 +0800

    spb-email:update mail code api

--- blog_springBoot/src/main/java/com/synergism/blog/email/controller/EmailController.java
+++ blog_springBoot/src/main/java/com/synergism/blog/email/controller/EmailController.java
@@ -3,11 +3,13 @@ package com.synergism.blog.email.controller;
 import com.synergism.blog.email.entity.CodeMail;
 import com.synergism.blog.email.service.MailService;
 import com.synergism.blog.email.utils.CodeUtil;
+import com.synergism.blog.exception.custom.MailErrorException;
 import com.synergism.blog.redis.service.RedisService;
 import com.synergism.blog.result.entity.CodeMsg;
 import com.synergism.blog.result.entity.Result;
 import com.synergism.blog.utils.TimeUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.mail.MessagingException;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -34,26 +34,34 @@ public class EmailController {
         this.redis = redis;
     }
 
+    //关闭强转类型编译提示
+    @SuppressWarnings("unchecked")
     @PostMapping("/code")
-    public Result<String> login(@RequestBody Map<String, String> mailMap) throws MessagingException, ParseException {
-        String mail = mailMap.get("mail");
-        Map map = (Map<String, String>) redis.getValue(mail);
-        if (map != null) {
-            CodeMail codeMail = CodeMail.getInstance(map);
-            if ((int) (new Date().getTime() - TimeUtil.toDate(codeMail.getTime()).getTime()) / 6000 > 1) {
-                //若redis中存在且记录的时间距离现在不到一分钟，返回请求过快
-                return Result.error(CodeMsg.ACCESS_LIMIT_REACHED);
-            }
+    public Result<String> getMailCode(@RequestBody Map<String, String> mailMap) throws MessagingException, ParseException {
+        try {
+            String mail = mailMap.get("mail");
+            Object object = redis.getValue(mail);
+            Map<String, String> dataMap;
+            //若redis中存在则判断时长，不存在则创建新map
+            if (object != null) {
+                dataMap = (Map<String, String>) object;
+                CodeMail codeMail = CodeMail.getInstance(dataMap);
+                if ((int) (new Date().getTime() - TimeUtil.toDate(codeMail.getTime()).getTime()) / 6000 > 1) {
+                    //记录的时间距离现在不到一分钟，返回请求过快
+                    return Result.error(CodeMsg.ACCESS_LIMIT_REACHED);
+                }
+            } else dataMap = new HashMap<>();
+
+            String code = CodeUtil.code();
+            dataMap.put("email", mail);
+            dataMap.put("code", code);
+            dataMap.put("createTime", TimeUtil.now());
+            service.sendTemplateMail(mail, "验证码", "registerTemplate", dataMap);
+            redis.getAndSetValue(mail, dataMap);
+            return Result.success();
+        } catch (
+                MailException e) {
+            throw new MailErrorException("发送失败");
         }
-
-
-        String code = CodeUtil.code();
-        Map<String, String> dataMap = new HashMap<>();
-        dataMap.put("email", mail);
-        dataMap.put("code", code);
-        dataMap.put("createTime", TimeUtil.now());
-        service.sendTemplateMail(mail, "验证码", "registerTemplate", dataMap);
-        redis.setEmail(mail, dataMap);
-        return Result.success();
     }
 }
