b864cf8b83bd478b9025eed90f7e57fe0d460558
Sysnergism612 <1273059247@qq.com>
2022-09-25 21:48:49 +0800

    spb-user:update close password encode

--- blog_springBoot/src/main/java/com/synergism/blog/blog/user/entity/User.java
+++ blog_springBoot/src/main/java/com/synergism/blog/blog/user/entity/User.java
@@ -6,8 +6,6 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.util.Date;
 
-import com.synergism.blog.security.entity.Power;
-import com.synergism.blog.utils.TypeUtil;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
@@ -45,7 +43,7 @@ public class User {
     @TableField("birthday")
     private Date birthday;
 
-    @ApiModelProperty("性别(0为女，1为男，3为未知)")
+    @ApiModelProperty("性别(0为未知，1为男，2为女)")
     @TableField("sex")
     private Integer sex;
 
@@ -53,14 +51,6 @@ public class User {
     @TableField("intro")
     private String intro;
 
-    @ApiModelProperty("状态码")
-    @TableField("status")
-    private Integer status;
-
-    @ApiModelProperty("权限")
-    @TableField("power")
-    private String power;
-
     /**
      * 构造函数
      * @param id id
@@ -71,10 +61,8 @@ public class User {
      * @param birthday 生日
      * @param sex 性别代码
      * @param intro 个人简介
-     * @param status 状态码
-     * @param power 权限信息(字符串)
      */
-    User(Long id, String icon, String name, String username, String password, Date birthday, Integer sex, String intro, Integer status, String power) {
+    User(Long id, String icon, String name, String username, String password, Date birthday, Integer sex, String intro) {
         this.id = id;
         this.icon = icon;
         this.name = name;
@@ -83,35 +71,6 @@ public class User {
         this.birthday = birthday;
         this.sex = sex;
         this.intro = intro;
-        this.status = status;
-        this.power = power;
-    }
-
-    /**
-     * 构造函数
-     * @param id id
-     * @param icon 头像
-     * @param name 昵称
-     * @param username 账号
-     * @param password 密码
-     * @param birthday 生日
-     * @param sex 性别代码
-     * @param intro 个人简介
-     * @param status 状态码
-     * @param power 权限信息(字符串数组)
-     */
-    User(Long id, String icon, String name, String username, String password, Date birthday, Integer sex, String intro, Integer status,
-                String[] power) {
-        this.id = id;
-        this.icon = icon;
-        this.name = name;
-        this.username = username;
-        this.password = password;
-        this.birthday = birthday;
-        this.sex = sex;
-        this.intro = intro;
-        this.status = status;
-        this.power = TypeUtil.arrayToString(power);
     }
 
     /**
@@ -120,6 +79,6 @@ public class User {
      * @return 用户
      */
     public static User getInstance(Register register){
-        return new User(null,"", register.getUsername(), register.getUsername(), register.getPassword(), null,0,null,1,Power.NOT_LOG_IN.getPower());
+        return new User(null,"", register.getUsername(), register.getUsername(), register.getPassword(), null,0,null);
     }
 }
