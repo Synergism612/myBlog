2403cfb9dfa340355e4324019c60353bff70fdd1
Sysnergism612 <1273059247@qq.com>
2022-09-27 23:56:17 +0800

    spb-main:update init

--- blog_springBoot/src/main/java/com/synergism/blog/BlogApplication.java
+++ blog_springBoot/src/main/java/com/synergism/blog/BlogApplication.java
@@ -3,14 +3,10 @@ package com.synergism.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import static com.synergism.blog.Init.init;
-
 @SpringBootApplication
 public class BlogApplication {
 
 	public static void main(String[] args) throws Exception {
-		//初始化
-		init();
 		//运行
 		SpringApplication.run(BlogApplication.class, args);
 	}
