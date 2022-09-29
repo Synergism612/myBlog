80207252d992d3c7d4ab2eefdb278aec3ac9bea0
Sysnergism612 <1273059247@qq.com>
2022-09-29 22:20:34 +0800

    spb-user:update import

--- blog_springBoot/src/main/java/com/synergism/blog/blog/user/controller/UserController.java
+++ blog_springBoot/src/main/java/com/synergism/blog/blog/user/controller/UserController.java
@@ -2,7 +2,8 @@ package com.synergism.blog.blog.user.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.synergism.blog.blog.user.entity.Register;
-import com.synergism.blog.email.note.EmailVerifyCodeNote;
+import com.synergism.blog.email.note.EmailCodeVerifyNote;
+import com.synergism.blog.exception.custom.RegisterFailException;
 import com.synergism.blog.result.entity.CodeMsg;
 import com.synergism.blog.result.entity.Result;
 import com.synergism.blog.blog.user.entity.Login;
@@ -46,8 +47,8 @@ public class UserController {
      * @return 结果[用户信息]
      */
     @PostMapping("/login")
-    @SessionManagementLoginNote //安全框架登录绑定会话注解
     @CryptographyPasswordNote //安全框架密码加密注解
+    @SessionManagementLoginNote //安全框架登录绑定会话注解
     public Result<UserInformation> login(@RequestBody Login login) {
         //获得对应用户
         User user = service.getOne(new QueryWrapper<User>().eq("username", login.getUsername()));
@@ -69,11 +70,11 @@ public class UserController {
      */
     @PostMapping("/register")
     @CryptographyPasswordNote //安全框架密码加密注解
-    @EmailVerifyCodeNote //邮箱验证码校验注解
+    @EmailCodeVerifyNote //邮箱验证码校验注解
     public Result<String> register(@RequestBody Register register) {
         //账号是否已存在判断
         if (service.ifExist(register.getUsername())) {
-            return Result.error(CodeMsg.REGISTER_ERROR.fillArgs("账号已存在"));
+            throw new RegisterFailException("账号已存在");
         }
         //保存到数据库
         service.save(User.getInstance(register));
