f31f3a795d039e9900bf2533b5c2b17311abe046
Sysnergism612 <1273059247@qq.com>
2022-09-29 22:19:59 +0800

    spb-security-cryptography:update aop encrypt password

--- blog_springBoot/src/main/java/com/synergism/blog/security/cryptography/aspect/CryptographyPasswordAspect.java
+++ blog_springBoot/src/main/java/com/synergism/blog/security/cryptography/aspect/CryptographyPasswordAspect.java
@@ -1,5 +1,6 @@
 package com.synergism.blog.security.cryptography.aspect;
 
+import com.synergism.blog.exception.custom.KeyFailureException;
 import com.synergism.blog.security.cryptography.service.CryptographyService;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -22,22 +23,34 @@ public class CryptographyPasswordAspect {
     }
 
     @Pointcut(value = "@annotation(com.synergism.blog.security.cryptography.note.CryptographyPasswordNote)")
-    public void CryptographyPassword(){}
+    public void CryptographyPassword() {
+    }
 
+    /**
+     * 获取参数列表
+     * 将参数名为password的参数值进行加密
+     *
+     * @param point 数据
+     * @return 参数
+     */
     @Around(value = "CryptographyPassword()")
-    public Object around(ProceedingJoinPoint point) throws Throwable {
-        Object[] args = point.getArgs();
-        for (Object arg : args) {
-            Class<?> argsClass = arg.getClass();
-            Field[] fields = argsClass.getDeclaredFields();
-            for (Field field : fields) {
-                if ("password".equals(field.getName())){
-                    field.setAccessible(true);
-                    String value =  (String) field.get(arg);
-                    field.set(arg,cryptographyService.MySQLEncrypt(value));
+    public Object around(ProceedingJoinPoint point){
+        try {
+            Object[] args = point.getArgs();
+            for (Object arg : args) {
+                Class<?> argsClass = arg.getClass();
+                Field[] fields = argsClass.getDeclaredFields();
+                for (Field field : fields) {
+                    if ("password".equals(field.getName())) {
+                        field.setAccessible(true);
+                        String value = (String) field.get(arg);
+                        field.set(arg, cryptographyService.MySQLEncrypt(value));
+                    }
                 }
             }
+            return point.proceed(args);
+        } catch (Throwable e) {
+            throw  new KeyFailureException("加密切面错误");
         }
-        return point.proceed(args);
     }
 }
--- blog_springBoot/src/main/java/com/synergism/blog/security/cryptography/note/CryptographyPasswordNote.java
+++ blog_springBoot/src/main/java/com/synergism/blog/security/cryptography/note/CryptographyPasswordNote.java
@@ -4,6 +4,7 @@ import java.lang.annotation.*;
 
 /**
  * 将密码加密
+ * 用以密码比对
  */
 @Documented
 @Target(ElementType.METHOD)
