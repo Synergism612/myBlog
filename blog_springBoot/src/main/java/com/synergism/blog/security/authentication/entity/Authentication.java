b229c3a9c96f9b9d07e7c137f6cf69cd22798032
Sysnergism612 <1273059247@qq.com>
2022-09-30 14:29:26 +0800

    spb-security-authentication:update init function

--- blog_springBoot/src/main/java/com/synergism/blog/security/authentication/entity/Authentication.java
+++ blog_springBoot/src/main/java/com/synergism/blog/security/authentication/entity/Authentication.java
@@ -1,10 +1,46 @@
 package com.synergism.blog.security.authentication.entity;
 
+import com.synergism.blog.blog.role.entity.Role;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
 public class Authentication {
 
+    private String name;
+    private int code;
+
+    public Authentication(String name, int code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    public static Authentication fromRole(Role role) {
+        return new Authentication(role.getName(), role.getCode());
+    }
+
+    public static List<Authentication> fromRoleList(List<Role> roleList) {
+       return roleList.stream().map(Authentication::fromRole).collect(Collectors.toList());
+    }
+
+    public static List<Authentication> init(){
+        List<Authentication> result = new ArrayList<>();
+        result.add(new Authentication("root",1));
+        result.add(new Authentication("user",2));
+
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Authentication{" +
+                "name='" + name + '\'' +
+                ", code=" + code +
+                '}';
+    }
 }
--- blog_springBoot/src/main/java/com/synergism/blog/security/authentication/serviceImpl/AuthenticationServiceImpl.java
+++ blog_springBoot/src/main/java/com/synergism/blog/security/authentication/serviceImpl/AuthenticationServiceImpl.java
@@ -1,8 +1,8 @@
 package com.synergism.blog.security.authentication.serviceImpl;
 
-import com.synergism.blog.blog.role.entity.Role;
 import com.synergism.blog.blog.role.service.RoleService;
 
+import com.synergism.blog.security.authentication.entity.Authentication;
 import com.synergism.blog.security.authentication.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,18 +12,18 @@ import java.util.List;
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
 
-    private List<Role> roleList;
+    private List<Authentication> authenticationList;
 
     private RoleService roleService;
 
     @Autowired
-    AuthenticationServiceImpl(RoleService roleService){
+    AuthenticationServiceImpl(RoleService roleService) {
         this.roleService = roleService;
     }
 
     @Override
     public void init() {
-        this.roleList = roleService.list();
-        roleList.forEach(System.out::println);
+        this.authenticationList = Authentication.fromRoleList(roleService.list());
+        authenticationList.forEach(U-> System.out.println(U.toString()));
     }
 }
