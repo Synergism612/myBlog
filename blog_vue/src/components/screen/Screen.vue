18be72dc3069169b08b860ce273860396632931a
Sysnergism612 <1273059247@qq.com>
2022-10-04 23:01:31 +0800

    vue-compoents:create screen

---
+++ blog_vue/src/components/screen/Screen.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="screen_container"></div>
+  <div id="screen_shade"></div>
+</template>
+<script lang="ts">
+import { defineComponent, onMounted, onUnmounted, reactive, toRefs } from "vue";
+export default defineComponent({
+  setup() {
+    const viewData = reactive({
+      top: 0,
+    });
+
+    const scrollTop = () => {
+      viewData.top = document.documentElement.scrollTop;
+      console.log(viewData.top);
+    };
+
+    onMounted(() => {
+      window.addEventListener("scroll", scrollTop);
+    });
+    onUnmounted(() => {
+      window.removeEventListener("scroll", scrollTop);
+    });
+    return { ...toRefs(viewData), scrollTop };
+  },
+});
+</script>
+<style lang="less">
+@import url("./Screen.less");
+</style>
