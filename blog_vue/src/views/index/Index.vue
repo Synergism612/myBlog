9c44d772a16447b054a4ee258dbd4b498d76ce3a
Sysnergism612 <1273059247@qq.com>
2022-10-04 23:02:52 +0800

    vue-index:create index vue

---
+++ blog_vue/src/views/index/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="index_background"></div>
+  <div id="index_container">
+    <Menu></Menu>
+    <Screen></Screen>
+    <div id="index_notice">1234567</div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from "vue";
+import Menu from "@/components/menu/Menu.vue";
+import Screen from "@/components/screen/Screen.vue";
+export default defineComponent({
+  setup() {
+    const viewData = reactive({});
+    return { ...toRefs(viewData) };
+  },
+  components: { Menu, Screen },
+});
+</script>
+<style lang="less">
+@import url("./Index.less");
+</style>
