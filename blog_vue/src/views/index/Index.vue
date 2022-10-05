294582c1e88ec86dd524f9125ef73b68185552a8
Sysnergism612 <1273059247@qq.com>
2022-10-05 23:12:38 +0800

    vue-index:update add notice

--- blog_vue/src/views/index/Index.vue
+++ blog_vue/src/views/index/Index.vue
@@ -1,9 +1,40 @@
 <template>
-  <div id="index_background"></div>
-  <div id="index_container">
-    <Menu></Menu>
-    <Screen></Screen>
-    <div id="index_notice">1234567</div>
+  <div class="box">
+    <div class="background">
+      <Menu></Menu>
+      <Screen></Screen>
+    </div>
+    <div class="container">
+      <el-container>
+        <el-header>
+          <el-row justify="center">
+            <el-col :sm="24" :md="20" class="notice frame">
+              <p>公告栏</p>
+              <p>公告内容</p>
+            </el-col>
+          </el-row>
+        </el-header>
+
+        <el-main>
+          <el-row justify="space-around">
+            <el-col :xs="24" :sm="24" :md="20" :span="20">
+              <el-row gutter="20" justify="space-between">
+                <el-col :xs="24" :sm="16" :md="16" :span="16">
+                  <div class="frame">
+                    <p>文章列表</p>
+                  </div>
+                </el-col>
+                <el-col :xs="0" :sm="8" :md="8" :span="8">
+                  <div class="frame">
+                    <p>右侧</p>
+                  </div>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+        </el-main>
+      </el-container>
+    </div>
   </div>
 </template>
 <script lang="ts">
