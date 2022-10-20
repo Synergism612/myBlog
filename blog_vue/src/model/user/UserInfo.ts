6979ef095eb3286a4166bba01d88a94e10441d9c
Synergism612 <1273059247@qq.com>
2022-10-20 23:16:31 +0800

    vue-model:update user nickname problem

--- blog_vue/src/model/user/UserInfo.ts
+++ blog_vue/src/model/user/UserInfo.ts
@@ -2,7 +2,7 @@ export default class UserInfo {
   //头像
   icon!: string;
   //昵称
-  name!: string;
+  nickname!: string;
   //账号
   username!: string;
   //生日
@@ -14,14 +14,14 @@ export default class UserInfo {
 
   constructor(
     icon?: string,
-    name?: string,
+    nickname?: string,
     username?: string,
     birthday?: Date,
     sex?: number,
     intro?: string
   ) {
     this.icon = icon || "";
-    this.name = name || "";
+    this.nickname = nickname || "";
     this.username = username || "";
     this.birthday = birthday || new Date();
     this.sex = sex || 0;
