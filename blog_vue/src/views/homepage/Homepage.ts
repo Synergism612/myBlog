41efcad2d566c19af0d4733ee55f07d4783859c0
Synergism612 <1273059247@qq.com>
2022-11-01 18:43:50 +0800

    vue-home:update basic framework

--- blog_vue/src/views/homepage/Homepage.ts
+++ blog_vue/src/views/homepage/Homepage.ts
@@ -1,3 +1,43 @@
-export default class Homepage{
-  
+import { api } from "@/api/api";
+import Author from "@/model/user/Author";
+import { store } from "@/store";
+
+export default class Homepage {
+  author: Author;
+  //本地账号
+  username: string;
+
+  changeShow: boolean;
+  //头像
+  icon: string;
+  //昵称
+  nickname: string;
+  //生日
+  birthday: Date;
+  //性别代码
+  sex: number;
+  //个人简介
+  intro: string;
+
+  constructor() {
+    this.author = new Author();
+    this.username = store.getters.getUser.username;
+    this.changeShow = false;
+    this.icon = "";
+    this.nickname = "";
+    this.birthday = new Date();
+    this.sex = 0;
+    this.intro = "";
+  }
+
+  public init(): void {
+    api.getHomepageAuthor(this.username).then(({ data }) => {
+      this.author = data;
+      this.icon = this.author.icon;
+      this.nickname = this.author.nickname;
+      this.birthday = this.author.birthday;
+      this.sex = this.author.sex;
+      this.intro = this.author.intro;
+    });
+  }
 }
--- blog_vue/src/views/homepage/Homepage.vue
+++ blog_vue/src/views/homepage/Homepage.vue
@@ -26,8 +26,78 @@
         <el-main>
           <!-- 主要内容 -->
           <el-row justify="space-around">
-            <!-- 主体框架 -->
-            <el-col :xs="24" :sm="24" :md="24" :lg="20"> </el-col>
+            <transition
+              appear
+              appear-active-class="animate__animated animate__zoomIn"
+            >
+              <!-- 主体框架 -->
+              <el-col :xs="24" :sm="24" :md="24" :lg="20">
+                <el-row>
+                  <el-col :span="18" class="info frame">
+                    <el-col :span="24">
+                      <el-row>
+                        <el-col :span="12">
+                          <el-col :span="24" class="icon">
+                            <el-avatar :size="200" :src="icon" />
+                            <div>更改头像</div>
+                          </el-col>
+                        </el-col>
+                        <el-col :span="12">
+                          <el-col :span="18" class="information nickname">
+                            <span>昵称：</span>
+                            <el-input
+                              v-model="author.nickname"
+                              placeholder="请输入昵称"
+                              clearable
+                            />
+                            <div class="information username">
+                              账号：
+                              {{ username }}
+                            </div>
+                            <div class="information birthday">
+                              生日：
+                              <el-date-picker
+                                v-model="birthday"
+                                type="date"
+                                placeholder="请选择"
+                              />
+                            </div>
+                            <div class="information sex">
+                              性别：
+                              <el-radio-group v-model="sex" class="ml-4">
+                                <el-radio :label="0">不愿透露</el-radio>
+                                <el-radio :label="1">男</el-radio>
+                                <el-radio :label="2">女</el-radio>
+                              </el-radio-group>
+                            </div>
+                            <div class="information intro">
+                              个人简介：
+                              <el-input
+                                v-model="intro"
+                                :rows="2"
+                                type="textarea"
+                                placeholder="请输入简介"
+                              />
+                            </div>
+                            <div class="information upToNow">
+                              园龄：{{ author.upToNow }}
+                            </div>
+                          </el-col>
+                          <el-col :span="24" class="information button">
+                            <span class="click">提交修改</span>
+                          </el-col>
+                          <el-col :span="24" class="information button">
+                            <span>我的文章</span>
+                            <span>我的关注</span>
+                            <span>我的粉丝</span>
+                          </el-col>
+                        </el-col>
+                      </el-row>
+                    </el-col>
+                  </el-col>
+                </el-row>
+              </el-col>
+            </transition>
           </el-row>
         </el-main>
       </el-container>
@@ -43,8 +113,9 @@ export default defineComponent({
   setup() {
     /**数据仓 */
     const viewData = reactive(new Homepage());
+
     onMounted(() => {
-      viewData;
+      viewData.init();
     });
 
     return {
