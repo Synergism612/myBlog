e36b5ce79b551b3beaf10808d9a2f4297f09ed77
Synergism612 <1273059247@qq.com>
2022-11-01 18:41:22 +0800

    spb-api:create homepage api

---
+++ blog_springBoot/src/main/java/com/synergism/blog/api/homepageAPI/service/HomepageAPIService.java
@@ -0,0 +1,22 @@
+package com.synergism.blog.api.enshrineAPI.service;
+
+import com.synergism.blog.api.enshrineAPI.entity.AddCollection;
+import com.synergism.blog.api.enshrineAPI.entity.AddFavoriteGroup;
+import com.synergism.blog.core.favorite.entity.Favorite;
+import com.synergism.blog.result.Result;
+
+import java.util.List;
+
+public interface EnshrineAPIService {
+
+    /**
+     * 根据账户获取收藏夹列表
+     * @param username 账户
+     * @return 收藏夹列表
+     */
+    Result<List<Favorite>> getFavorite(String username);
+
+    Result<String> setFavorite(AddFavoriteGroup addFavoriteGroup);
+
+    Result<String> setCollection(AddCollection addCollection);
+}
---
+++ blog_springBoot/src/main/java/com/synergism/blog/api/homepageAPI/serviceImpl/HomepageAPIServiceImpl.java
@@ -0,0 +1,59 @@
+package com.synergism.blog.api.enshrineAPI.serviceImpl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.synergism.blog.api.enshrineAPI.entity.AddCollection;
+import com.synergism.blog.api.enshrineAPI.entity.AddFavoriteGroup;
+import com.synergism.blog.api.enshrineAPI.service.EnshrineAPIService;
+import com.synergism.blog.core.collection.entity.Collection;
+import com.synergism.blog.core.collection.service.CollectionService;
+import com.synergism.blog.core.favorite.entity.Favorite;
+import com.synergism.blog.core.favorite.service.FavoriteService;
+import com.synergism.blog.core.user.service.UserService;
+import com.synergism.blog.result.CodeMsg;
+import com.synergism.blog.result.Result;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EnshrineAPIServiceImpl implements EnshrineAPIService {
+
+    private final UserService userService;
+    private final FavoriteService favoriteService;
+    private final CollectionService collectionService;
+
+    public EnshrineAPIServiceImpl(UserService userService, FavoriteService favoriteService, CollectionService collectionService) {
+        this.userService = userService;
+        this.favoriteService = favoriteService;
+        this.collectionService = collectionService;
+    }
+
+    @Override
+    public Result<List<Favorite>> getFavorite(String username) {
+        long userID = userService.getID(username);
+        if (userID == -1) return Result.error(CodeMsg.BIND_ERROR.fillArgs("用户错误"));
+        return Result.success(favoriteService.getFavoriteListByUserID(userID));
+    }
+
+    @Override
+    public Result<String> setFavorite(AddFavoriteGroup addFavoriteGroup) {
+        long userID = userService.getID(addFavoriteGroup.getUsername());
+        if (userID == -1) return Result.error(CodeMsg.BIND_ERROR.fillArgs("用户错误"));
+        return favoriteService.save(addFavoriteGroup.getName(), addFavoriteGroup.getAnnotation(), addFavoriteGroup.getIf_private(), userID)
+                ? Result.success()
+                : Result.error(CodeMsg.BIND_ERROR.fillArgs("创建失败"));
+    }
+
+    @Override
+    public Result<String> setCollection(AddCollection addCollection) {
+        if (favoriteService.isExist(addCollection.getFavoriteID())) {
+            return Result.error(CodeMsg.BIND_ERROR.fillArgs("收藏夹中已经存在该收藏"));
+        }
+        if (collectionService.isExist(addCollection.getFavoriteID(), addCollection.getHref())) {
+            return collectionService.save(addCollection.getTitle(), addCollection.getHref(), addCollection.getSynopsis(), addCollection.getFavoriteID())
+                    ? Result.success()
+                    : Result.error(CodeMsg.BIND_ERROR.fillArgs("收藏夹错误"));
+        }
+        return Result.error(CodeMsg.BIND_ERROR.fillArgs("收藏夹错误"));
+    }
+}
