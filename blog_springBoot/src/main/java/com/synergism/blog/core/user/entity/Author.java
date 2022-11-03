8f9347d6797a7ba732213e79bb81ba50c3abbf6d
Synergism612 <1273059247@qq.com>
2022-11-03 23:08:34 +0800

    spb-core:update user annotation

--- blog_springBoot/src/main/java/com/synergism/blog/core/user/entity/Author.java
+++ blog_springBoot/src/main/java/com/synergism/blog/core/user/entity/Author.java
@@ -3,11 +3,17 @@ package com.synergism.blog.core.user.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * 作者信息
+ */
 @Setter
 @Getter
 public class Author extends UserInformation {
+    //所属文章数量
     private int articleCount;
+    //关注数量
     private int notableCount;
+    //粉丝数量
     private int fansCount;
 
     public Author(UserInformation userInformation,AuthorInformation authorInformation) {
