cacf9695579bc08c9b90bcd2461d0ab8f58d3b79
Synergism612 <1273059247@qq.com>
2022-11-04 20:12:31 +0800

    spb-result:update BIND_ERROR

--- blog_springBoot/src/main/java/com/synergism/blog/result/CodeMsg.java
+++ blog_springBoot/src/main/java/com/synergism/blog/result/CodeMsg.java
@@ -14,7 +14,7 @@ public class CodeMsg {
     //通用的错误码
     public static CodeMsg SUCCESS = new CodeMsg(200, "成功");
     public static CodeMsg SERVER_ERROR = new CodeMsg(500100, "服务端异常");
-    public static CodeMsg BIND_ERROR = new CodeMsg(500101, "参数校验异常：%s");
+    public static CodeMsg BIND_ERROR = new CodeMsg(500101, "校验异常：%s");
     public static CodeMsg REQUEST_ILLEGAL = new CodeMsg(500102, "请求非法：%s");
     public static CodeMsg ACCESS_LIMIT_REACHED = new CodeMsg(500103, "访问太频繁！");
     public static CodeMsg KEY_FAILURE = new CodeMsg(500104, "密钥失效：%s");
