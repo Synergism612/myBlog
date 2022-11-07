b8aac44d0794b0c412a45da02d03f73a42322d21
Synergism612 <1273059247@qq.com>
2022-11-07 23:20:26 +0800

    vue-config:update

--- blog_vue/vite.config.ts
+++ blog_vue/vite.config.ts
@@ -5,9 +5,10 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "./",
   resolve: {
     alias: {
-      "src": resolve(__dirname, "src"), // 路径别名
+      src: resolve(__dirname, "src"), // 路径别名
     },
     extensions: [".js", ".json", ".ts"], // 使用路径别名时想要省略的后缀名，可以自己 增减
   },
@@ -16,12 +17,5 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 8080,
     https: false,
-    // proxy: {
-    //   "/api": {
-    //     target: "http://localhost:8088/",
-    //     changeOrigin: true,
-    //     rewrite: (path) => path.replace(/^\/api/, ""), // 不可以省略rewrite
-    //   },
-    // },
   },
 });
