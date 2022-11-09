c0e73fc790ec494fefb897bdd22d7cf92f6bfac3
Synergism612 <1273059247@qq.com>
2022-11-09 09:48:51 +0800

    vue-confug:update base url and add preview

--- blog_vue/vite.config.ts
+++ blog_vue/vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "./",
+  base: "/",
   resolve: {
     alias: {
       src: resolve(__dirname, "src"), // 路径别名
