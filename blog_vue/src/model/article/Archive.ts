f07b9a68ac83d2bd202d9eb03144842021eada29
Synergism612 <1273059247@qq.com>
2022-11-10 22:08:25 +0800

    vue-article:update archive

--- blog_vue/src/model/article/Archive.ts
+++ blog_vue/src/model/article/Archive.ts
@@ -1,13 +1,11 @@
 import Article from "./Article";
 
 export default class Archive {
-  year: string;
   month: string;
   date: string;
   articleList: Array<Article>;
 
   constructor(){
-    this.year = "";
     this.month = "";
     this.date = "";
     this.articleList = [new Article()];
