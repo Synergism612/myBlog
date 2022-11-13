7b23a881d99f7d123f23465e26f985b67570b055
Synergism612 <1273059247@qq.com>
2022-11-13 22:04:03 +0800

    vue-axios:update returns

--- blog_vue/src/axios/axios.ts
+++ blog_vue/src/axios/axios.ts
@@ -34,67 +34,71 @@ class Axios {
   constructor() {
     this.service = axios.create(this.config);
     //请求拦截
-    this.service.interceptors.request.use((request: AxiosRequestConfig) => {
-      console.log("请求拦截" + request.url);
-      console.log("请求拦截-参数" + JSON.stringify(request.params));
-      console.log("请求拦截-数据" + JSON.stringify(request.data));
-      if (request && request.headers) {
-        // 多一步判断
-        request.headers["ANOTHER_WORLD_KEY"] = store.state.ANOTHER_WORLD_KEY;
-        request.headers["EVIL_EYE"] = store.state.EVIL_EYE;
-      }
-      // 对 data 进行加密
-      request.data = AESUtil.encrypt(
-        JSON.stringify(request.data),
-        store.state.KEY
-      );
-
-      // 对 params 进行加密
-      request.params = {
-        params: AESUtil.encrypt(
-          JSON.stringify(request.params),
+    this.service.interceptors.request.use(
+      (request: AxiosRequestConfig): AxiosRequestConfig => {
+        console.log("请求拦截" + request.url);
+        console.log("请求拦截-参数" + JSON.stringify(request.params));
+        console.log("请求拦截-数据" + JSON.stringify(request.data));
+        if (request && request.headers) {
+          // 多一步判断
+          request.headers["ANOTHER_WORLD_KEY"] = store.state.ANOTHER_WORLD_KEY;
+          request.headers["EVIL_EYE"] = store.state.EVIL_EYE;
+        }
+        // 对 data 进行加密
+        request.data = AESUtil.encrypt(
+          JSON.stringify(request.data),
           store.state.KEY
-        ),
-      };
-      return request;
-    });
+        );
+
+        // 对 params 进行加密
+        request.params = {
+          params: AESUtil.encrypt(
+            JSON.stringify(request.params),
+            store.state.KEY
+          ),
+        };
+        return request;
+      }
+    );
 
     //响应拦截
-    this.service.interceptors.response.use((response: AxiosResponse) => {
-      let result;
-      //前端对应的安全策略
-      const EVIL_EYE = response.headers["evil_eye"];
-      console.log("响应拦截邪王--" + EVIL_EYE);
-      //判空，写入邪王真眼
-      if (!StringUtil.checkStringIfEmpty(EVIL_EYE))
-        store.commit("SET_EVIL_EYE", EVIL_EYE);
-      //检查异世界钥是否为空
-      if (StringUtil.checkStringIfEmpty(store.state.ANOTHER_WORLD_KEY)) {
-        result = Result.getResult(response);
-      } else {
-        try {
-          //对响应的数据解密
-          const json = JSON.parse(
-            AESUtil.decrypt(response.data, store.state.KEY)
-          );
-          console.log(json);
-          result = new Result(json.code, json.msg, json.time, json.data);
-          //全局异常处理
-        } catch {
+    this.service.interceptors.response.use(
+      (response: AxiosResponse): AxiosResponse => {
+        let result;
+        //前端对应的安全策略
+        const EVIL_EYE = response.headers["evil_eye"];
+        console.log("响应拦截邪王--" + EVIL_EYE);
+        //判空，写入邪王真眼
+        if (!StringUtil.checkStringIfEmpty(EVIL_EYE))
+          store.commit("SET_EVIL_EYE", EVIL_EYE);
+        //检查异世界钥是否为空
+        if (StringUtil.checkStringIfEmpty(store.state.ANOTHER_WORLD_KEY)) {
           result = Result.getResult(response);
+        } else {
+          try {
+            //对响应的数据解密
+            const json = JSON.parse(
+              AESUtil.decrypt(response.data, store.state.KEY)
+            );
+            console.log(json);
+            result = new Result(json.code, json.msg, json.time, json.data);
+            //全局异常处理
+          } catch {
+            result = Result.getResult(response);
+          }
         }
-      }
-      if (result.code != 200) {
-        if (result.code == 12008) {
-          Message.warningMessage(result.msg);
+        if (result.code != 200) {
+          if (result.code == 12008) {
+            Message.warningMessage(result.msg);
+            throw new Error(result.msg);
+          }
+          Message.errorMessage(result.msg);
           throw new Error(result.msg);
         }
-        Message.errorMessage(result.msg);
-        throw new Error(result.msg);
+        response.data = result.data;
+        return response;
       }
-      response.data = result.data;
-      return response;
-    });
+    );
   }
 }
 
