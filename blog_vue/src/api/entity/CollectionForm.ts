c5e4406dd4a812e0f531939ff077f3ea8c9d50e2
Synergism612 <1273059247@qq.com>
2022-11-15 19:42:31 +0800

    vue-api:update CollectionForm

--- blog_vue/src/api/entity/CollectionForm.ts
+++ blog_vue/src/api/entity/CollectionForm.ts
@@ -1,10 +1,13 @@
+import { store } from "src/store";
 export default class CollectionForm {
+  username: string;
   title: string;
   href: string;
   synopsis: string;
   favoriteID: number;
 
   constructor() {
+    this.username = store.getters.getUsername;
     this.title = "";
     this.href = "";
     this.synopsis = "";
