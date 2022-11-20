52d0a816ba1fb7a69944ab29baec095c8175165e
Synergism612 <1273059247@qq.com>
2022-11-20 08:57:17 +0800

    spb-test:delete

--- blog_springBoot/src/test/java/com/synergism/blog/BlogApplicationTests.java
+++ blog_springBoot/src/test/java/com/synergism/blog/BlogApplicationTests.java
@@ -15,30 +15,5 @@ import java.io.File;
 
 @SpringBootTest
 class BlogApplicationTests {
-    Logger logger = LoggerFactory.getLogger(BlogApplicationTests.class);
 
-    private final String separator = File.separator;
-
-    private final EmailService emailService;
-    private final IOService ioService;
-    private final FolderService folderService;
-
-    @Autowired
-    BlogApplicationTests(EmailService emailService, IOService ioService, FolderService folderService) {
-        this.emailService = emailService;
-        this.ioService = ioService;
-        this.folderService = folderService;
-    }
-
-    @Test
-    public void test() throws MessagingException {
-        emailService.sendCodeMail("1273059247@qq.com",new CodeMail("1273059247@qq.com","1234","2022"));
-    }
-
-    @Test
-    public void testIO()  {
-        String path = "111@qq.com"+separator+"blog"+separator+"icon";
-        folderService.update(1l,path);
-        ioService.mkdir(path);
-    }
 }
